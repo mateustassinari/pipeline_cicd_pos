07ed80fc382476eccf52dbc62f07514f628c7b39
Mateus Tassinari Ferreira <mateustassinari18@gmail.com>
2023-04-02 10:56:13 -0300

    teste pipeline teste unitario falhando

--- app.js
+++ app.js
@@ -1,12 +1,73 @@
 const express = require('express');
 
 const app = express();
-const createError = require('http-errors');
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/hello', (req, res) => res.send('Hello World!'));
+app.get('/',  (req, res) => {
+    res.redirect('/users');
+});
+
+app.get('/users', (req, res) => {
+
+    let users = [{
+        name:'User 1',
+        email: 'user1@email.com',
+        id:1
+    },{
+        name:'User 2',
+        email: 'user2@email.com',
+        id:2
+    },
+    {
+        name:'User 3',
+        email: 'user3@email.com',
+        id:3
+    }];
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type','application/json');
+    res.end(JSON.stringify({
+        users
+    }));
+   
+});
+
+app.get('/users/:id', (req, res) => {
+
+    let users = [{
+        name:'User 1',
+        email: 'user1@email.com',
+        id:1
+    },{
+        name:'User 2',
+        email: 'user2@email.com',
+        id:2
+    },
+    {
+        name:'User 3',
+        email: 'user3@email.com',
+        id:3
+    }];
+
+    let id = req.params.id;
+
+    let user = users.filter(u => u.id == id);
+
+    if(user.length == 0) {
+        res.statusCode = 400;
+        res.end('User not found');
+        return;
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type','application/json');
+    res.end(JSON.stringify({
+        user
+    }));
+   
+});
+
 
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -6,14 +6,21 @@ const agent = supertest.agent(app);
 
 describe('simple get request', () => {
   it('should return 200 ok', async () => {
-    const response = await agent.get('/hello').send();
+    const response = await agent.get('/users').send();
     expect(response.status).toEqual(200);
   });
 });
 
 describe('simple get request 404', () => {
-  it('should return 404 ok', async () => {
+  it('should return 404', async () => {
     const response = await agent.get('/test').send();
     expect(response.status).toEqual(404);
+  });
+});
+
+describe('simple get request 400 id not found', () => {
+  it('should return 400', async () => {
+    const response = await agent.get('/users/5').send();
+    expect(response.status).toEqual(200);
   });
 });
